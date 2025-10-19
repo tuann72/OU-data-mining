5f87937f885c3cc41af30300bb9f371c866a0ffb
TenHam3 <simbaandbella3@gmail.com>
2025-10-19 02:03:06 -0500

    Modified preprocessor to make it a binary classification problem

--- preprocessor.py
+++ preprocessor.py
@@ -1,7 +1,19 @@
 import csv
 
+numRows, drops, nonDrops = 0, 0, 0
 with open("data.csv", "r", newline="") as infile, open("clean_data.csv", "w", newline="") as outfile:
     reader = csv.reader(infile, delimiter=";")
     writer = csv.writer(outfile)
     for row in reader:
+        numRows += 1
+        if row[-1] == "Dropout":
+            row[-1] = 0
+            drops += 1
+        else:
+            row[-1] = 1
+            nonDrops += 1
         writer.writerow(row)
+
+print(f"Dropout Rate: {drops/numRows}")
+print(f"Non-Dropout Rate: {nonDrops/numRows}")
+print(f"Total Rows: {numRows}")
