d9579000a694f2b4b033500ac296dc95e7377505
TenHam3 <simbaandbella3@gmail.com>
2025-10-19 05:15:28 -0500

    Implemented decision tree construction

---
+++ possible_values.py
@@ -0,0 +1,20 @@
+import csv
+
+categorical_indices = {i for i in range(37)}
+numerical_indices = {6, 12, 19, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35}
+categorical_indices = categorical_indices - numerical_indices
+
+possible_values = {i: set() for i in categorical_indices}
+
+with open("clean_data.csv", 'r') as f:
+    reader = csv.reader(f, delimiter=",")
+    next(reader)
+    for row in reader:
+        for i in categorical_indices:
+            if row[i] not in possible_values[i]:
+                possible_values[i].add(int(row[i]))
+
+print("{")
+for k, v in possible_values.items():
+    print(f"{k}: {sorted(v)},")
+print("}")
