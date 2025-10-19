9ca0c26e887f948d39a350b2c4d60fc9f335eda6
TenHam3 <simbaandbella3@gmail.com>
2025-10-19 14:04:26 -0500

    Minor improvement of code and fixed comment

--- decisiontrees.py
+++ decisiontrees.py
@@ -3,15 +3,14 @@ from helper import *
 
 # Read in data
 data = np.loadtxt('clean_data.csv', delimiter=',', skiprows=1)
-# Get number of examples m and dimensionality n (last column is target so decrement n)
+# Get number of examples m and dimensionality n
 m, n = np.shape(data)
-n -= 1
 
 # Split dataset into training and testing
 train_set, test_set = split_data(data)
 
-categorical_indices = {i for i in range(37)}
+categorical_indices = {i for i in range(n)}
 numerical_indices = {6, 12, 19, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35}
 categorical_indices = categorical_indices - numerical_indices
 
-tree = construct_tree(train_set, [i for i in range(37)], categorical_indices)
+tree = construct_tree(train_set, [i for i in range(n)], categorical_indices)
