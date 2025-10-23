7b323f9eb0072cf90af0fbdd727c5389ba5ff3d5
Vishnu Patel <viptl123@gmail.com>
2025-10-23 15:11:16 -0500

    Added plotting and metrics

--- logistic_regression/helper.py
+++ logistic_regression/helper.py
@@ -82,7 +82,7 @@ def train_logistic_regression(data, learning_rate, iterations):
 def make_prediction(weights, bias, instance):
     z = np.dot(instance, weights) + bias
     if sigmoid(z) >= 0.5:
-        return 1 
+        return 1
     else:
         return 0
 
@@ -103,3 +103,112 @@ def get_accuracy(weights, bias, test_set):
         if y_prediction == y_true:
             correct += 1
     return correct / len(test_set)
+
+
+# Calculate precision recall curve
+def calculate_precision_recall_curve(weights, bias, test_set):
+    y_confidence = []
+    y_true = []
+
+    # get predictions and true labels for each example
+    for example in test_set:
+        true_label = int(example[-1])
+        prediction = predict_probability(weights, bias, example[:-1])
+        y_confidence.append(prediction)
+        y_true.append(true_label)
+
+    y_confidence = np.array(y_confidence)
+    y_true = np.array(y_true)
+
+    # sort by confidence 
+    sorted_indices = np.argsort(-y_confidence)
+    y_true = y_true[sorted_indices]
+    y_confidence = y_confidence[sorted_indices]
+
+    # initialize values 
+    true_positive, false_positive = 0, 0
+    false_negative = np.sum(y_true)
+    precision, recall = [], []
+
+   
+    for i in range(len(y_true)):
+        # update tp fn and fp counts
+        if y_true[i] == 1:
+            true_positive += 1
+            false_negative -= 1
+        else:
+            false_positive += 1
+
+        # calculate precision and recall avoiding division of 0
+        if (true_positive + false_positive) > 0:
+            curr_prec = true_positive / (true_positive + false_positive)
+        else:
+            curr_prec = 1.0
+        
+        if (true_positive + false_negative) > 0:
+            curr_rec = true_positive / (true_positive + false_negative) 
+        else:
+            curr_rec = 0.0
+
+        precision.append(curr_prec)
+        recall.append(curr_rec)
+    # Calculate area under curve
+    auc = np.trapezoid(precision, recall)
+    return precision, recall, auc
+
+
+# Calculate ROC curve
+def calculate_roc_curve(weights, bias, test_set):
+    y_confidence = []
+    y_true = []
+
+
+    # get predictions and true labels for each example
+    for instance in test_set:
+        true_label = int(instance[-1])
+        confidence = predict_probability(weights, bias, instance[:-1])
+        y_confidence.append(confidence)
+        y_true.append(true_label)
+
+    y_confidence = np.array(y_confidence)
+    y_true = np.array(y_true)
+    # sort by confidence
+    sorted_indices = np.argsort(-y_confidence)
+    y_true = y_true[sorted_indices]
+    y_confidence = y_confidence[sorted_indices]
+
+    # initialize values
+    true_positive = 0
+    false_positive = 0
+    false_negative = np.sum(y_true)
+    true_negative = len(y_true) - false_negative
+
+    tpr_list = []
+    fpr_list = []
+
+    # Update counts for tp, fn, fp, and tn
+    for i in range(len(y_true)):
+        if y_true[i] == 1:
+            true_positive += 1
+            false_negative -= 1
+        else:
+            false_positive += 1
+            true_negative -= 1
+
+        # calculate true positive rate and false positive rate avoiding division by 0
+        if (true_positive + false_negative) > 0:
+            tpr = true_positive / (true_positive + false_negative) 
+        else:
+            tpr = 0.0
+        
+        if (false_positive + true_positive) > 0:
+            fpr = false_positive / (false_positive + true_negative) 
+        else:
+            fpr = 0.0
+
+        tpr_list.append(tpr)
+        fpr_list.append(fpr)
+    # Calculate area under curve
+    auc = np.trapezoid(tpr_list, fpr_list)
+    return fpr_list, tpr_list, auc
+
--- logistic_regression/logisticregression.py
+++ logistic_regression/logisticregression.py
@@ -1,5 +1,6 @@
 import numpy as np
 from helper import *
+import matplotlib.pyplot as plt
 
 # Load data and split into training and testing sets
 # skip first row cause of column headers
@@ -13,3 +14,25 @@ model_weights, model_biases = train_logistic_regression(updated_array, 0.05,1000
 # get model accuracy
 model_accuracy = get_accuracy(model_weights, model_biases, np.array(testing_set))
 print(f"Model Accuracy: {model_accuracy: .3f}")
+
+
+# Calculate and plot precision recall curve
+model_precision, model_recall, auc_pr = calculate_precision_recall_curve(model_weights, model_biases, testing_set)
+plt.plot(model_recall, model_precision, label=f'AUC = {auc_pr: .3f}')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Logistic Regression Precision Recall Curve')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Calulate and plot ROC Curve
+false_positive_rate, true_positive_rate, auc_roc = calculate_roc_curve(model_weights, model_biases, testing_set)
+plt.plot(false_positive_rate, true_positive_rate, label=f'AUC = {auc_roc: .3f}')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Logistic Regression ROC Curve')
+plt.legend()
+plt.grid(True)
+plt.show()
+
