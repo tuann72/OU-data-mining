543298936aeea87ba759550c1420b38c02eee4d9
Vishnu Patel <viptl123@gmail.com>
2025-10-23 13:51:52 -0500

    Added model and training functions

--- logistic_regression/helper.py
+++ logistic_regression/helper.py
@@ -26,3 +26,55 @@ def split_data(data):
     # return training set and testing set
     return np.array(training_set), np.array(testing_set)
 
+
+
+
+# sigmoid function for logistic regression model
+def sigmoid(z):
+    denominator = (1 + np.exp(-z))
+    return 1 / denominator
+
+# Train logistic regression model using gradient descent
+def train_logistic_regression(data, learning_rate, iterations):
+    X = data[:, :-1]
+    y = data[:, -1]
+    m, n = X.shape
+
+    #initialize empty array for weights and biases to 0
+    weights = np.zeros(n)
+    biases = 0.0
+
+    # for each iteration update weights and biases
+    for iter in range(iterations):
+        # dot product of X and weights add biases
+        z = np.dot(X, weights) + biases
+        # use sigmoid function to make a label prediction
+        y_prediction = sigmoid(z)
+
+
+        error = y_prediction - y
+
+        # Compute gradients
+        gradient_weight = (X.T @ error) / m
+        gradient_bias = np.mean(error)
+
+        # Update weights and biases using gradients
+        weights -= learning_rate * gradient_weight
+        biases  -= learning_rate * gradient_bias
+
+        # Every 100 iterations calculate and print loss
+        if iter % 100 == 0:
+            # use small constant to avoid log(0)
+            epsilon = 1e-8
+
+            # Calculate cross-entropy loss
+            positive_class_loss = y * np.log(y_prediction + epsilon)
+            negative_class_loss = (1 - y) * np.log(1 - y_prediction + epsilon)
+
+            # Calculate the average loss across all examples
+            loss = -np.mean(positive_class_loss + negative_class_loss)
+
+            # Print loss
+            print(f"Iteration number: {iter:3d}  Loss: {loss: .3f}")
+
+    return weights, biases
--- logistic_regression/logisticregression.py
+++ logistic_regression/logisticregression.py
@@ -5,3 +5,7 @@ from helper import *
 # skip first row cause of column headers
 data = np.loadtxt('../clean_data.csv', delimiter=',', skiprows=1)
 training_set, testing_set = split_data(data)
+
+# Train logistic regression model
+updated_array = np.array(training_set)
+model_weights, model_biases = train_logistic_regression(updated_array, 0.05,1000)
